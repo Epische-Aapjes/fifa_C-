59dec731704a1c5cf9c4314046cfeb34fff75fbe
w9009 <woutermevius@live.nl>
2017-05-18 09:57:17 +0200

    latest version

--- C#/ProjectFifaV2/ProjectFifaV2/frmAdmin.Designer.cs
+++ C#/ProjectFifaV2/ProjectFifaV2/frmAdmin.Designer.cs
@@ -108,6 +108,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
             this.ClientSize = new System.Drawing.Size(959, 512);
             this.Controls.Add(this.btnAdminLogOut);
             this.Controls.Add(this.dgvAdminData);
